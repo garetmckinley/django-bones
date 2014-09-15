4cda6d68761422b3a5b7f9e6bc8691bed78160a2
Garet McKinley <garetmckinley@me.com>
2014-09-15 17:59:59 -0400

    initial commit of bones. Project is now official

---
+++ bones/functions.py
@@ -0,0 +1,42 @@
+from django.template import RequestContext, loader
+from django.conf import settings
+import re
+import urllib
+
+
+def loadTemplate(file):
+    template = loader.get_template('%s/%s.jade' % (settings.BONES_TEMPLATE,
+                                                   file))
+    return template
+
+
+def whichTemplate():
+    return settings.BONES_TEMPLATE
+
+
+def getTemplateStatic():
+    return '%sbones/templates/%s' % (settings.STATIC_URL, whichTemplate())
+
+
+def getModernizrDict(request):
+    get = request.COOKIES.get('modernizr')
+    url = urllib.unquote(get).decode('utf8')
+    # print(url)
+    values = re.split('=|&', url)
+    modernizr = {}
+    keys = [x for ind, x in enumerate(values) if ind % 2 == 0]
+    values = [x for ind, x in enumerate(values) if ind % 2 != 0]
+    s = []
+    for x in xrange(0, len(keys)):
+        subvalues = re.findall(r"\[([A-Za-z0-9_]+)\]", keys[x])
+        if len(subvalues) > 0:
+            subvalue = subvalues[0]
+            key = keys[x].replace("[%s]" % subvalue, "")
+            try:
+                modernizr[key][subvalue] = int(values[x])
+            except:
+                modernizr[key] = {subvalue: int(values[x])}
+        else:
+            key = keys[x]
+            modernizr[key] = int(values[x])
+    return modernizr
---
+++ bones/views.py
@@ -0,0 +1,16 @@
+from django.template import RequestContext
+from django.http import HttpResponse
+from django.shortcuts import render
+import functions
+
+# Create your views here.
+
+
+def index(request):
+    modernizr = functions.getModernizrDict(request)
+    template = functions.loadTemplate("index")
+    context = RequestContext(request, {
+        'TEMPLATE_STATIC': functions.getTemplateStatic(),
+        'modernizr': modernizr
+    })
+    return HttpResponse(template.render(context))
