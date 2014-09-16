4b2307b142f787114cc477309997a6b1b1c942fe
Garet McKinley <garetmckinley@me.com>
2014-09-16 02:10:28 -0400

    added generic BonesObjectAdmin ModelAdmin

--- bones/admin.py
+++ bones/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from bones.models import BonesObject, Post, BonesProfile
+
+
+class BonesObjectAdmin(admin.ModelAdmin):
+    """Default ModelAdmin for BonesObject"""
+    prepopulated_fields = {"slug": ("title",)}
+
 
 # Register your models here.
+admin.site.register(Post, BonesObjectAdmin)
+admin.site.register(BonesProfile)
