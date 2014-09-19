899f9f27e6efca7f1e6962d9e38c490c4e946ec2
Garet McKinley <garetmckinley@me.com>
2014-09-19 01:12:56 -0400

    Added template URLs for post templates

--- bones/urls.py
+++ bones/urls.py
@@ -1,7 +1,17 @@
 from django.conf.urls import patterns, url
 
 from bones import views
+from bones.models import Page
 urlpatterns = patterns(
     '',
     url(r'^$', views.index, name='index'),
 )
+
+templates = Page.objects.filter()
+print(templates)
+for template in templates:
+    additional_settings = patterns(
+        '',
+        (r'^template/' + template.slug + '/$', 'template'),
+    )
+    urlpatterns += additional_settings
