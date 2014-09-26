cb7543a3beb5d278ca1b27e6cce4512f64a08736
Garet McKinley <garetmckinley@me.com>
2014-09-26 00:31:52 -0400

    Added dynamic post url schemes

--- bones/urls.py
+++ bones/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, url
 
 from bones import views
-from bones.models import Page
+from bones.models import Page, Category
 urlpatterns = patterns(
     '',
     url(r'^$', views.index, name='index'),
@@ -15,3 +15,16 @@ for template in templates:
         (r'^template/' + template.slug + '/$', 'template'),
     )
     urlpatterns += additional_settings
+
+
+categories = Category.objects.all()
+
+for category in categories:
+    additional_settings = patterns(
+        '',
+        url(r'^' + category.slug + '/(?P<post_slug>.*)/$',
+            views.singlepost, name='singlepost'),
+    )
+    urlpatterns += additional_settings
+
+print(urlpatterns)
