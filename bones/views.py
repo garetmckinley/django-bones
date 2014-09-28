6583d8c3986a5cc6e5563be88920530e42ad84b8
Garet McKinley <garetmckinley@me.com>
2014-09-28 03:20:26 -0400

    Custom JS and CSS gets loaded now

--- bones/views.py
+++ bones/views.py
@@ -14,9 +14,15 @@ def index(request):
 
 def singlepost(request, post_slug):
     post = Post.objects.get(slug__exact=post_slug)
+    body = ""
+    if post.css_output:
+        body += "<style>%s</style>" % post.css_output
+    body += post.content_html
+    if post.javascript_output:
+        body += "<script type='text/javascript'>%s</script>" % post.javascript_output
     properties = {
         'TITLE': post.title,
-        'BODY': mark_safe(post.content_html),
+        'BODY': mark_safe(body),
         'CATEGORY': post.category,
         'POST_DATE': post.post_date,
         'CREATED_DATE': post.created,
